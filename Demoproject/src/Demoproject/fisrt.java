b2859bfa86aeed4d77c84563a5333ee2e659c16e
Hello <Hello@DESKTOP-7FE9KK7>
2023-10-01 19:24:48 +0530

    first commit

--- Demoproject/src/Demoproject/fisrt.java
+++ Demoproject/src/Demoproject/fisrt.java
@@ -1,9 +1,10 @@
-package demoproject;
+package Demoproject;
 
-public class first {
+public class fisrt {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome");
+
 	}
 
 }
--- Demoproject/src/module-info.java
+++ Demoproject/src/module-info.java
@@ -4,5 +4,5 @@
 /**
  * 
  */
-module demoproject {
+module Demoproject {
 }
